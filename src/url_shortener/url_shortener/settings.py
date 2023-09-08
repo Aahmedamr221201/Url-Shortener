1746f9d320aa21933d8be45f44729ca066af5ee8
Ahmed Amr <aahmedamr221201@aucegypt.edu>
2023-09-08 10:47:05 +0300

    edit allowed hosts

--- src/url_shortener/url_shortener/settings.py
+++ src/url_shortener/url_shortener/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://shooooort.club/']
+ALLOWED_HOSTS = ['https://64fadec6eb8c510007cad66f--smarturlshortener.netlify.app/']
 
 
 # Application definition
